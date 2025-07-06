96b2d01f0fdb67dfdf2ce4bbee38bf8580e3eaaf
Maxi <128001873+maxiarias86@users.noreply.github.com>
2025-07-06 16:28:16 -0300

    Pendiente terminar form new user

--- src/main/java/com/example/SabioDivisor/controller/UserController.java
+++ src/main/java/com/example/SabioDivisor/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.SabioDivisor.controller;
 
+import com.example.SabioDivisor.model.AppUser;
 import com.example.SabioDivisor.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,12 @@ public class UserController {
     @GetMapping
     public String list(Model model) {
         model.addAttribute("users", service.listAll());
-        return "user-list"; // Esto apunta a la vista Thymeleaf en src/main/resources/templates/users/list.html
+        return "users/list"; // Esto apunta a la vista Thymeleaf en src/main/resources/templates/users/list.html
+    }
+
+    @GetMapping
+    public String add(Model model) {
+        model.addAttribute("user", new AppUser()); // Metodo para crear un nuevo usuario
+        return "users/form"; // Esto apunta a la vista Thymeleaf en src/main/resources/templates/users/form.html
     }
 }
